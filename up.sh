52f981bb600db4ec1d769cb649602bd84e0d6537
Your Name <you@example.com>
2025-02-25 10:13:46 +0000

    save

--- up.sh
+++ up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod 777 database
 docker network create --attachable mynet
 docker-compose -f efk.compose.yml -p rbmdkrfinalefk up -d --build
 docker-compose -f app.compose.yml -p rbmdkrfinalapp up -d --build
