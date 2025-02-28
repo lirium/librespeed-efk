706134f39c15c68a0d65c13769e738c0e72134e3
alex <liriumone@gmail.com>
2025-02-28 12:20:38 +0000

    save

--- up.sh
+++ up.sh
@@ -3,4 +3,5 @@ mkdir database
 chmod 777 database
 docker network create --attachable mynet
 docker-compose -f efk.compose.yml -p rbmdkrfinalefk up -d --build
+sleep 10
 docker-compose -f app.compose.yml -p rbmdkrfinalapp up -d --build
